563479b4d20b605d87c40da688fc7042a106bcd7
Sahil Yadav <sahilyadavind0908@gmail.com>
2025-02-03 17:51:24 +0530

    c1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  build: { outDir: "dist" },
   // server: {
   //   proxy: {
   //     "/api": {
